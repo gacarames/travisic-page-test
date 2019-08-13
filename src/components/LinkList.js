04542041e2298d3d04ef03894e9434c96c7f6a30
gcarames <gacarames@gmail.com>
2019-08-13 15:46:37 -0300

    First Deploy app

---
+++ src/components/LinkList.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Link from "./Link";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+class LinkList extends Component {
+  render() {
+    const FEED_QUERY = gql`
+      {
+        feed {
+          links {
+            id
+            createdAt
+            url
+            description
+            postedBy {
+              id
+              name
+            }
+            votes {
+              id
+              user {
+                id
+              }
+            }
+          }
+        }
+      }
+    `;
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map((link, index) => (
+                <Link key={link.id} link={link} index={index} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default LinkList;
