b0bff9c831b74ed071fbb8fb5448ac03eb179ff0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-20 19:04:58 +0000

    Deploy gacarames/travisic-page-test to github.com/gacarames/travisic-page-test.git:gh-pages

---
+++ precache-manifest.00cd9f966c2ce7c545e4a0fe0edbc916.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c63e98d2d52e346aba99863c6df13f8",
+    "url": "/travisic-page-test/index.html"
+  },
+  {
+    "revision": "a311afaa270a7d9da0e6",
+    "url": "/travisic-page-test/static/css/main.bd4835e8.chunk.css"
+  },
+  {
+    "revision": "01b43577070dfd18d741",
+    "url": "/travisic-page-test/static/js/2.841fb988.chunk.js"
+  },
+  {
+    "revision": "a311afaa270a7d9da0e6",
+    "url": "/travisic-page-test/static/js/main.a6f3462a.chunk.js"
+  },
+  {
+    "revision": "c1ef14cf24ff7812f23c",
+    "url": "/travisic-page-test/static/js/runtime~main.8d7fa1c9.js"
+  }
+]);
